c5d9e559505e9089f939aebda2f4dab66ba899bd
Ross Brodbeck <hross@github.com>
2020-05-18 16:02:09 -0400

    fix some linting errors

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import * as core from '@actions/core';
+import * as core from '@actions/core'
 
 import {IRetroArguments, tryCreateRetro} from './retro'
 
@@ -6,14 +6,13 @@ async function run(): Promise<void> {
   try {
     const args: IRetroArguments = {
       repoToken: core.getInput('repo-token', {required: true}),
-      handles: [],//core.getInput('repo-token', {required: true}),
+      handles: [], //core.getInput('repo-token', {required: true}),
       retroCadenceInWeeks: parseInt(core.getInput('retro-cadence-weeks')) ?? 1,
       retroDayOfWeek: parseInt(core.getInput('retro-day-of-week')) ?? 0,
       onlyLog: core.getInput('only-log') === 'true'
     }
 
-    await tryCreateRetro(args);
-
+    await tryCreateRetro(args)
   } catch (error) {
     core.setFailed(error.message)
   }
